03c521126b1404b6ec21975ba4052a7a4c9859dd
PhillipHenry <PhillHenry@gmail.com>
2023-09-17 10:58:01 +0100

    Non random distribution

--- pysynic/synthetic_data.py
+++ pysynic/synthetic_data.py
@@ -20,3 +20,23 @@ def random_from(xs: list, seed: int = None) -> Optional[str]:
         index = seed % len(xs)
     return xs[index]
 
+
+def randomly_null(x, seed: int = None, mod=2):
+    """
+
+    :param x: The value that will (potentially) become None
+    :param seed: An optional value that makes the output deterministic
+    :param mod: The value returned will be None every mod times, either on average or strictly
+    deterministically depending on the value of seed
+    :return: either x or None
+    """
+    if seed is not None:
+        if seed % mod == 1:
+            return None
+        else:
+            return x
+    else:
+        if random.randint(1, mod) == 1:
+            return None
+        else:
+            return x
--- test/test_synthetic_data.py
+++ test/test_synthetic_data.py
@@ -1,22 +1,42 @@
 from pysynic.synthetic_data import random_from
-import numpy as np
+
+sample_size = 10
+samples = list(range(sample_size))
 
 
 def test_random_from_list_with_seed():
-    n = 10
-    xs = list(range(n))
-    m = 20
+    num_samples = 20
     results = []
-    for i in range(n * m):
-        results.append(random_from(xs, i))
-    assert set(results) == set(xs)
-    check_uniform_distribution(results, xs, m)
+    for i in range(sample_size * num_samples):
+        results.append(random_from(samples, i))
+    assert set(results) == set(samples)
+    counts = check_uniform_distribution(results, samples, num_samples)
+    assert len(set(counts)) == 1
 
 
-def check_uniform_distribution(results: list, xs: list, expected_count: int):
+def test_random_from_list_based_on_probabilities():
+    num_samples = 100
+    results = []
+    for i in range(sample_size * num_samples):
+        results.append(random_from(samples))
+    check_non_uniform_distribution(results)
+
+
+def check_non_uniform_distribution(results: list):
+    counts = set()
+    for x in samples:
+        counts.add(count_of(x, results))
+    assert len(counts) > 1
+
+
+def check_uniform_distribution(results: list, xs: list, expected_count: int) -> [int]:
+    counts = []
     for i in xs:
-        assert count_of(i, results) == expected_count
+        count = count_of(i, results)
+        assert count == expected_count
+        counts.append(count)
+    return counts
     
 
-def count_of(seek, xs) -> int:
+def count_of(seek, xs: list) -> int:
     return len([x for x in xs if x == seek])
